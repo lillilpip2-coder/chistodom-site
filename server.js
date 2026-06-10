74b0b5ed69d18353b2111576bb1ae20ffc384b90
dev <dev@mail.com>
2026-06-10 22:24:14 +0300

    House background with dark overlay, clean layout

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.post('/api/contact', contactLimiter, (req, res) => {
   res.json({ success: true, message: 'Заявка принята! Мы свяжемся с вами в ближайшее время.' });
 });
 
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
